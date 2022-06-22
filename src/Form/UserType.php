7b16bf3e6eb782f3715d392f4989d45957b771b9
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2022-06-23 00:55:53 +0200

    Fix roles form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -16,10 +18,24 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
-            ->add('roles', UserType::class,  ['label' => 'Rôles'])
+            ->add('roles', ChoiceType::class,  ['choices' => ['Utilisateur' => 'ROLE_USER', 'Administrateur' => 'ROLE_ADMIN'], 'label' => 'Rôles'])
             ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
             ->add('email', EmailType::class, ['label' => 'Email'])
         ;
+
+        // Data transformer
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                     // transform the array to a string
+                     return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                     // transform the string back to an array
+                     return [$rolesString];
+                }
+        ));
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void

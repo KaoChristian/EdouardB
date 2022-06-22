a89a1982a36d0d40f6047a826f7da40c470d0ec7
KaoChristian <kao.c@hotmail.fr>
2022-06-22 20:36:00 +0200

    update admin controllers

--- src/Controller/AdminGalleryController.php
+++ src/Controller/AdminGalleryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Gallery;
+use App\Form\GalleryType;
 use App\Repository\GalleryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +20,52 @@ class AdminGalleryController extends AbstractController
         return $this->render('admin/gallery/index.html.twig', [
             'galleries' => $galleryRepository->findAll(),
         ]);
+    }
+
+    #[Route('/admin/galleries/new', name: 'app_sections_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, GalleryRepository $sectionRepository): Response
+    {
+        $gallery = new Gallery();
+        $form = $this->createForm(SectionType::class, $gallery);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sectionRepository->add($gallery, true);
+
+            return $this->redirectToRoute('app_galleries', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/galleries/new.html.twig', [
+            'gallery' => $gallery,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/galleries/{id}/edit', name: 'app_sections_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Gallery $gallery, GalleryRepository $galleryRepository): Response
+    {
+        $form = $this->createForm(SectionType::class, $gallery);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $galleryRepository->add($gallery, true);
+
+            return $this->redirectToRoute('app_galleries', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/galleries/edit.html.twig', [
+            'gallery' => $gallery,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/galleries/{id}', name: 'app_sections_delete', methods: ['POST'])]
+    public function delete(Request $request, Gallery $gallery, GalleryRepository $galleryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$gallery->getId(), $request->request->get('_token'))) {
+            $galleryRepository->remove($gallery, true);
+        }
+
+        return $this->redirectToRoute('app_galleries', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/AdminSectionCategoriesController.php
+++ src/Controller/AdminSectionCategoriesController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\SectionCategory;
+use App\Form\SectionCategoryType;
 use App\Repository\SectionCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminSectionCategoriesController extends AbstractController
+class AdminSectionCategoryCategoriesController extends AbstractController
 {
-    #[Route('/admin/section-categories', name: 'app_section_categories')]
+    #[Route('/admin/section_categories', name: 'app_section_categories')]
     public function index(SectionCategoryRepository $sectionCategoryRepository): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
@@ -17,5 +20,52 @@ class AdminSectionCategoriesController extends AbstractController
         return $this->render('admin/section_categories/index.html.twig', [
             'section_categories' => $sectionCategoryRepository->findAll(),
         ]);
+    }
+
+    #[Route('/admin/section_categories/new', name: 'app_section_categories_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, SectionCategoryRepository $sectioncategoryRepository): Response
+    {
+        $section_category = new SectionCategory();
+        $form = $this->createForm(SectionCategoryType::class, $section_category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sectioncategoryRepository->add($section_category, true);
+
+            return $this->redirectToRoute('app_sections', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/section_categories/new.html.twig', [
+            'section_category' => $section_category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/section_categories/{id}/edit', name: 'app_section_categories_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, SectionCategory $section_category, SectionCategoryRepository $sectioncategoryRepository): Response
+    {
+        $form = $this->createForm(SectionCategoryType::class, $section_category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sectioncategoryRepository->add($section_category, true);
+
+            return $this->redirectToRoute('app_sections', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/section_categories/edit.html.twig', [
+            'section_category' => $section_category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/section_categories/{id}', name: 'app_section_categories_delete', methods: ['POST'])]
+    public function delete(Request $request, SectionCategory $section_category, SectionCategoryRepository $sectioncategoryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$section_category->getId(), $request->request->get('_token'))) {
+            $sectioncategoryRepository->remove($section_category, true);
+        }
+
+        return $this->redirectToRoute('app_section_categories', [], Response::HTTP_SEE_OTHER);
     }
 }

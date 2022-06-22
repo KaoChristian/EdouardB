94b7f09f0c367756104a8e0e0106cacced98b5e3
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2022-06-22 19:10:24 +0200

    Add users list

--- src/Controller/AdminArticlesController.php
+++ src/Controller/AdminArticlesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminArticlesController extends AbstractController
 {
     #[Route('/admin/articles', name: 'app_articles')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->render('admin/articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
--- src/Controller/AdminUsersController.php
+++ src/Controller/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminUsersController extends AbstractController
 {
     #[Route('/admin/users', name: 'app_users')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->render('admin/users/index.html.twig', [
-            'controller_name' => 'UsersController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }

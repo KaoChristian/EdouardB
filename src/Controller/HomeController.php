4240d50dbd234945bc6ce98e56be57ae9ac06f94
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2022-06-23 03:25:41 +0200

    Add data to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CarouselRepository;
+use App\Repository\ArticleRepository;
+use App\Repository\InfoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CarouselRepository $carouselRepository, ArticleRepository $articleRepository, InfoRepository $infoRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'carousels' => $carouselRepository->findAll(),
+            'carousels' => $articleRepository->findAll(),
+            'carousels' => $infoRepository->findAll(),
+        ]);
     }
 }

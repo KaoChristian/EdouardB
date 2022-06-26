1da8a73c8ea19f305e1c441e9f6ce8e6076bf204
KaoChristian <kao.c@hotmail.fr>
2022-06-26 05:44:36 +0200

    update Controllers & views

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\CarouselRepository;
+use App\Repository\SectionRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\InfoRepository;
-use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +13,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CarouselRepository $carouselRepository, ArticleRepository $articleRepository, InfoRepository $infoRepository): Response
+    public function index(CarouselRepository $carouselRepository, SectionRepository $sectionRepository, ArticleRepository $articleRepository, InfoRepository $infoRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'carousels' => $carouselRepository->findAll(),
+            'sections' => $sectionRepository->findAll(),
             'articles' => $articleRepository->findAll(),
             'infos' => $infoRepository->findAll(),
         ]);
